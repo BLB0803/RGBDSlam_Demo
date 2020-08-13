5ddf4d876a027f3a16d01d9a857d780d6b412e76
BLB_0803 <shizhiwen1992@gmail.com>
2020-08-13 17:54:14 +0900

    deleted

--- build/slam/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;std_msgs;image_transport;sensor_msgs;cv_bridge;message_filters;geometry_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "slam"
-PROJECT_SPACE_DIR = "/home/demo/slam_with_yolo/devel"
-PROJECT_VERSION = "0.0.0"
